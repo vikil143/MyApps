f52a64ccc23677d774f0a8fd6b8f7a3f44681c2b
vikil_netcore <vikil.lakkavatri@netcorecloud.com>
2024-10-20 17:59:24 +0530

    calulator init work

---
+++ src/apps/calculator/plan.ts
@@ -0,0 +1,19 @@
+/*
+    # Calculator Planning
+    Functionality of this app
+    1. User types input box ==> Whether its was number inputs or operator to do some calulation
+    2. And Do Calulation of the given inputs
+    3. Clean the all calculation
+
+    # Data requirements
+    Static and Dynamic Data
+
+    react state or redux state
+
+    # React Component Design Pattern Practices
+    1. Presentation Components 
+    ==> Number and Operator Box ==> because this can static and Single component while passing the data
+
+    2. Container component
+    ==> 
+*/
